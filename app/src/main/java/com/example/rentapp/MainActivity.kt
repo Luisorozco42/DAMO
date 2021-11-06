9c931890a395afeddb3ec56ed74dcb1e07ec36e5
Luisorozco42 <lorozcolazo@gmail.com>
2021-11-05 23:15:02 -0600

    Colores

--- app/src/main/java/com/example/rentapp/MainActivity.kt
+++ app/src/main/java/com/example/rentapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.rentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rentapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
